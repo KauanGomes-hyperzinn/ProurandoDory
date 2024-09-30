216c84f5ada7c1e393ef311c5b6e702afc10cd22
Kauan Gomes <kauangoliveira2008@gmail.com>
2024-09-30 17:36:56 -0300

    procurandoadory

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿﻿Console.Clear();
+
+Console.WriteLine("Imitando dory");
+string frase = Console.ReadLine()!;
+
+string FraseEmBaleies = frase
+
+ .Replace("a", "aaa" )
+ .Replace("A", "AAA" )
+ .Replace("e", "eee" )
+ .Replace("E", "EEE" )
+ .Replace("i", "iii" )
+ .Replace("I", "III" )
+ .Replace("o", "ooo" )
+ .Replace("O", "OOO" )
+ .Replace("u", "uuu")
+ .Replace("U", "UUU")
+;
+
+Console.WriteLine($"\n em baleiês:\n\n {FraseEmBaleies}");
